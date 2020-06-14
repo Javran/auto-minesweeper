8f7029c1f23b797f01dba90b9e02aae7c390b72f
Javran Cheng <Javran.C@gmail.com>
2020-06-14 01:33:59 -0700

    recognize from screenshot.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # (WIP) Automatic solver for Google minesweeper easter egg
 
-Requires `opencv-python`.
+Requirements:
+- xwininfo
+- opencv-python
+- ImageMagick
 
+Make sure project is executed from repository root so that assets are loaded correctly.
--- find_window.py
+++ find_window.py
@@ -6,17 +6,42 @@ import re
 import subprocess
 
 import cv2
-
+import numpy
 
 RE_XWININFO_WIN_ID_EXTRACT = re.compile(r'^xwininfo: Window id: (\d+) ', re.MULTILINE)
+PAT_HARD = cv2.imread('assets/hard.jpg',cv2.IMREAD_COLOR)
+
+
+def canny(img):
+    return cv2.Canny(img,0,1)
 
 
 def find_window_id():
+    """Finds window id."""
     print('Left click to select a window that has minesweeper running ...')
     result = subprocess.run(['xwininfo', '-int'],
                             stdout=subprocess.PIPE, check=True, encoding='utf8')
     return int(RE_XWININFO_WIN_ID_EXTRACT.search(result.stdout).group(1))
 
 
+def recognize_minesweeper(window_id, img_filter=lambda x: x):
+    """Recognizes minesweeper game and return position of top-left corner."""
+    result = subprocess.run(['import',
+                             '-window', str(window_id),
+                             'png:-'],
+                            stdout=subprocess.PIPE, check=True)
+    nparr = numpy.frombuffer(result.stdout, numpy.uint8)
+    screenshot = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img, pat = img_filter(screenshot), img_filter(PAT_HARD)
+    res = cv2.matchTemplate(img, pat, cv2.TM_CCORR_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(res)
+    print(max_val)
+    assert max_val >= 0.994, 'Cannot find minesweeper in the selected window.'
+    pat_x, pat_y = max_loc
+    # TODO: make this more customization by attaching some metadata to the pattern asset.
+    return (pat_x - 12, pat_y + 49)
+
+
 if __name__ == '__main__':
-    print(find_window_id())
+    window_id = find_window_id()
+    recognize_minesweeper(window_id)
